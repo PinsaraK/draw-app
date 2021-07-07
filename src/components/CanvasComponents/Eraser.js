7ff31b1f4782f13808b46b41e7c0685262c9c7fa
PinsaraK <pinsarakuruppu@gmail.com>
2021-07-07 17:38:37 -0400

    adjusted styles on selectors

--- src/components/CanvasComponents/Eraser.js
+++ src/components/CanvasComponents/Eraser.js
@@ -1,10 +1,14 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CanvasContext } from "../../store/CanvasContext";
 
 const Eraser = () => {
   const { erase } = useContext(CanvasContext);
 
-  return <button onClick={erase}>Eraser</button>;
+  return (
+    <div className="eraser" onClick={erase}>
+      Eraser
+    </div>
+  );
 };
 
 export default Eraser;
