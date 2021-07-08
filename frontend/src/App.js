4024f3ad1e039861c6defc4a3bb4c417ce87aaea
PinsaraK <pinsarakuruppu@gmail.com>
2021-07-08 19:20:33 -0400

    added socketio connections

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Canvas from "./components/Canvas";
 import CanvasOptions from "./components/CanvasOptions";
+import { io } from "socket.io-client";
 
 function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:8080", {
+      transports: ["websocket", "polling", "flashsocket"],
+    });
+
+    return () => {
+      socket.close();
+    };
+  }, []);
+
   return (
     <div className="container">
       <Canvas />
