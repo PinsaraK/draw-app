31ddf1ef7d8dbb60a4cde8665a138968bfb80750
PinsaraK <pinsarakuruppu@gmail.com>
2021-08-17 16:49:42 -0400

    added an alert when user tries to refresh page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Canvas from "./components/Canvas";
 import CanvasOptions from "./components/CanvasOptions";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
+
+  const handleBeforeUnload = (e) => {
+    e.preventDefault();
+    alert("hello");
+    const message =
+      "The canvas will not be saved, and you will leave any rooms you have joined or hosted.";
+    e.returnValue = message;
+    return message;
+  };
+
   return (
     <div className="container">
       <Canvas />
